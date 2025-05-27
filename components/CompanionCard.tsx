8500491aff4b27d80a6afaadf2f7001ccd0c1130
kaushik0010 <paykolikaushikb@gmail.com>
2025-05-27 23:26:40 +0530

    clerk authentication and supabase implementation

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -28,7 +28,7 @@ const CompanionCard = ({id, name, topic, subject, duration, color}: CompanionCar
             <Image src="/icons/clock.svg" alt="duration" width={13.5} height={13.5} />
             <p className="texsm">{duration} minutes</p>
         </div>
-        <Link href={`/companion/${id}`} className="w-full">
+        <Link href={`/companions/${id}`} className="w-full">
             <button className="btn-primary w-full justify-center">
                 Launch Lesson
             </button>
